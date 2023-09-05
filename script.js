2c12b05597d74a8e20375446ea2c4e4fd783a6da
Felipe Fransa <antoniofelipesilva@live.com>
2023-09-05 18:12:30 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let firstName;
+let currentAge;
+
+firstName = "Felipe"
+currentAge = 26
+
+let ageVerification = currentAge >= 18;
+let verificationString = firstName === String;
+
+
+console.log(ageVerification)
+console.log(typeof firstName)

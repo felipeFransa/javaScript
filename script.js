e85e1eb995760c9774b2a24a9f2291fd2fb97d01
Felipe Fransa <antoniofelipesilva@live.com>
2023-09-13 17:46:19 -0300

    add

--- script.js
+++ script.js
@@ -7,6 +7,23 @@ currentAge = 26
 let ageVerification = currentAge >= 18;
 let verificationString = firstName === String;
 
+let weight;
+
 
 console.log(ageVerification)
 console.log(typeof firstName)
+console.log(weight)
+
+let cars = ["BMW", "Toyota", "Honda", "Fiat"]
+
+let newPerson = {
+    name: "Fransa", age: 27, weight: 66
+}
+
+weight = newPerson.weight;
+currentAge = newPerson.age;
+firstName = newPerson.name;
+
+console.log(firstName)
+console.log(currentAge)
+console.log(weight)
